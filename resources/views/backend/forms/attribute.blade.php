9493a5a19e20eb40b6698137f406fa1d5f098b35
Abdelrahman Omran <me@omranic.com>
2017-06-22 00:47:48 +0200

    Add slugify support

--- resources/views/backend/forms/attribute.blade.php
+++ resources/views/backend/forms/attribute.blade.php
@@ -55,7 +55,7 @@
                                     {{-- Name --}}
                                     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                                         {{ Form::label('name', trans('cortex/attributable::common.name'), ['class' => 'control-label']) }}
-                                        {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => trans('cortex/attributable::common.name'), 'required' => 'required', 'autofocus' => 'autofocus']) }}
+                                        {{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => trans('cortex/attributable::common.name'), 'data-slugify' => '#slug', 'required' => 'required', 'autofocus' => 'autofocus']) }}
 
                                         @if ($errors->has('name'))
                                             <span class="help-block">{{ $errors->first('name') }}</span>
