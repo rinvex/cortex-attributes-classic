8958a20c06cfe0e4d9eae314bc29108f11249eb7
Abdelrahman Omran <me@omranic.com>
2017-08-02 17:26:40 +0200

    Add breadcrumbs

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Cortex\Attributable\Models\Attribute;
+use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator;
+
+Breadcrumbs::register('backend.attributes.index', function (BreadcrumbsGenerator $breadcrumbs) {
+    $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.trans('cortex/foundation::common.backend'), route('backend.home'));
+    $breadcrumbs->push(trans('cortex/attributable::common.attributes'), route('backend.attributes.index'));
+});
+
+Breadcrumbs::register('backend.attributes.create', function (BreadcrumbsGenerator $breadcrumbs) {
+    $breadcrumbs->parent('backend.attributes.index');
+    $breadcrumbs->push(trans('cortex/attributable::common.create_attribute'), route('backend.attributes.create'));
+});
+
+Breadcrumbs::register('backend.attributes.edit', function (BreadcrumbsGenerator $breadcrumbs, Attribute $attribute) {
+    $breadcrumbs->parent('backend.attributes.index');
+    $breadcrumbs->push($attribute->name, route('backend.attributes.edit', ['attribute' => $attribute]));
+});
+
+Breadcrumbs::register('backend.attributes.logs', function (BreadcrumbsGenerator $breadcrumbs, Attribute $attribute) {
+    $breadcrumbs->parent('backend.attributes.index');
+    $breadcrumbs->push($attribute->name, route('backend.attributes.edit', ['attribute' => $attribute]));
+    $breadcrumbs->push(trans('cortex/attributable::common.logs'), route('backend.attributes.logs', ['attribute' => $attribute]));
+});
