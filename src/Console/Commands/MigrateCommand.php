5e04802373c0a40819379fdb00ea9039128381b4
Abdelrahman Omran <me@omranic.com>
2017-08-13 03:25:37 +0200

    Add migration and seed artisan commands

---
+++ src/Console/Commands/MigrateCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Attributable\Console\Commands;
+
+use Rinvex\Attributable\Console\Commands\MigrateCommand as BaseMigrateCommand;
+
+class MigrateCommand extends BaseMigrateCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cortex:migrate:attributable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate Cortex Attributable Tables.';
+}
---
+++ src/Console/Commands/SeedCommand.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Attributable\Console\Commands;
+
+use Illuminate\Console\Command;
+use Cortex\Fort\Traits\AbilitySeeder;
+use Cortex\Fort\Traits\BaseFortSeeder;
+use Illuminate\Support\Facades\Schema;
+
+class SeedCommand extends Command
+{
+    use AbilitySeeder;
+    use BaseFortSeeder;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cortex:seed:attributable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Seed Default Cortex Attributable data.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->ensureExistingAttributableTables()) {
+            // No seed data at the moment!
+        }
+
+        if ($this->ensureExistingFortTables()) {
+            $this->seedAbilities(realpath(__DIR__.'/../../../resources/data/abilities.json'));
+        }
+    }
+
+    /**
+     * Ensure existing attributable tables.
+     *
+     * @return bool
+     */
+    protected function ensureExistingAttributableTables()
+    {
+        if (! $this->hasAttributableTables()) {
+            $this->call('cortex:migrate:attributable');
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if all required attributable tables exists.
+     *
+     * @return bool
+     */
+    protected function hasAttributableTables()
+    {
+        return Schema::hasTable(config('rinvex.attributable.tables.attributes'))
+               && Schema::hasTable(config('rinvex.attributable.tables.attribute_entity'))
+               && Schema::hasTable(config('rinvex.attributable.tables.attribute_boolean_values'))
+               && Schema::hasTable(config('rinvex.attributable.tables.attribute_datetime_values'))
+               && Schema::hasTable(config('rinvex.attributable.tables.attribute_integer_values'))
+               && Schema::hasTable(config('rinvex.attributable.tables.attribute_text_values'))
+               && Schema::hasTable(config('rinvex.attributable.tables.attribute_varchar_values'));
+    }
+}
