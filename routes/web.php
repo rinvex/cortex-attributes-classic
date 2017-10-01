e442eedfefc40a1126a0a28bba66a6afe9780fa3
Abdelrahman Omran <me@omranic.com>
2017-10-01 18:14:45 +0200

    Tweak routes registration

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-Route::group(['domain' => domain()], function () {
+Route::domain(domain())->group(function () {
 
     Route::name('adminarea.')
          ->namespace('Cortex\Attributes\Http\Controllers\Adminarea')
