96838967988d951c72e0261d7556933b0ba9a0a2
Abdelrahman Omran <me@omranic.com>
2017-12-18 18:48:20 +0200

    Refactor menus

--- routes/menus.php
+++ routes/menus.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
-Menu::adminareaSidebar('resources')->routeIfCan('list-attributes', 'adminarea.attributes.index', '<i class="fa fa-leaf"></i> <span>'.trans('cortex/attributes::common.attributes').'</span>');
+use Rinvex\Menus\Models\MenuItem;
+use Rinvex\Menus\Factories\MenuFactory;
+
+Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
+    $menu->findBy('title', trans('cortex/foundation::common.cms'), function (MenuItem $dropdown) {
+        $dropdown->route(['adminarea.attributes.index'], trans('cortex/attributes::common.attributes'), 10, 'fa fa-leaf')->can('list-attributes');
+    });
+});
