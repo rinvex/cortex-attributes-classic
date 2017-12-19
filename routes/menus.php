3e9b10b3ad38d58082a5230abbb22e295c4b34c8
Abdelrahman Omran <me@omranic.com>
2017-12-19 17:43:29 +0200

    Tweak menus

--- routes/menus.php
+++ routes/menus.php
@@ -6,7 +6,7 @@ use Rinvex\Menus\Models\MenuItem;
 use Rinvex\Menus\Factories\MenuFactory;
 
 Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
-    $menu->findBy('title', trans('cortex/foundation::common.cms'), function (MenuItem $dropdown) {
+    $menu->findByTitleOrAdd(trans('cortex/foundation::common.cms'), 40, 'fa fa-file-text-o', [], function (MenuItem $dropdown) {
         $dropdown->route(['adminarea.attributes.index'], trans('cortex/attributes::common.attributes'), 10, 'fa fa-leaf')->can('list-attributes');
     });
 });
