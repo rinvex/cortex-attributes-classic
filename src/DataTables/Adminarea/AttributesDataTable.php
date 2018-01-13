14b352bcde7c1e2a713a78c96f12634ff1a8dea5
Abdelrahman Omran <me@omranic.com>
2018-01-13 21:48:46 +0200

    Add datatables 'retrieve' attribute to prevent reinitialization issues

--- src/DataTables/Adminarea/AttributesDataTable.php
+++ src/DataTables/Adminarea/AttributesDataTable.php
@@ -42,6 +42,7 @@ class AttributesDataTable extends AbstractDataTable
     {
         return [
             'keys' => true,
+            'retrieve' => true,
             'autoWidth' => false,
             'dom' => "<'row'<'col-sm-6'B><'col-sm-6'f>> <'row'r><'row'<'col-sm-12't>> <'row'<'col-sm-5'i><'col-sm-7'p>>",
             'buttons' => [
