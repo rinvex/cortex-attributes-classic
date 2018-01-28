b941d1f0803612f7967bffb44f66759331fe939f
Abdelrahman Omran <me@omranic.com>
2018-01-28 12:40:44 +0200

    Simplify DataTables

--- src/DataTables/Adminarea/AttributesDataTable.php
+++ src/DataTables/Adminarea/AttributesDataTable.php
@@ -15,6 +15,28 @@ class AttributesDataTable extends AbstractDataTable
     protected $model = Attribute::class;
 
     /**
+     * {@inheritdoc}
+     */
+    protected $builderParameters = [
+        'drawCallback' => 'function (settings) {
+            var lastGroup = null;
+            var api = this.api();
+            var colspan = api.columns(\':visible\').count();
+            var rows = api.rows({page:\'current\'}).nodes();
+
+            api.column(\'group:name\', {page:\'current\'} ).data().each(function (rowGroup, rowIndex) {
+                if (lastGroup !== rowGroup) {
+                    $(rows).eq(rowIndex).before(
+                        \'<tr class="attribute-group"><td colspan="\'+colspan+\'"><strong>\'+(rowGroup ? rowGroup : "No Group")+\'</strong></td></tr>\'
+                    );
+ 
+                    lastGroup = rowGroup;
+                }
+            });
+        }',
+    ];
+
+    /**
      * Get the query object to be processed by dataTables.
      *
      * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder|\Illuminate\Support\Collection
@@ -25,42 +47,6 @@ class AttributesDataTable extends AbstractDataTable
         $query = app($this->model)->query()->orderBy('group', 'ASC')->orderBy('sort_order', 'ASC')->orderBy("name->\${$locale}", 'ASC');
 
         return $this->applyScopes($query);
-    }
-
-    /**
-     * Get default builder parameters.
-     *
-     * @return array
-     */
-    protected function getBuilderParameters(): array
-    {
-        return [
-            'keys' => true,
-            'retrieve' => true,
-            'autoWidth' => false,
-            'dom' => "<'row'<'col-sm-6'B><'col-sm-6'f>> <'row'r><'row'<'col-sm-12't>> <'row'<'col-sm-5'i><'col-sm-7'p>>",
-            'buttons' => [
-                ['extend' => 'create', 'text' => '<i class="fa fa-plus"></i> '.trans('cortex/foundation::common.new')], 'print', 'reset', 'reload', 'export',
-                ['extend' => 'colvis', 'text' => '<i class="fa fa-columns"></i> '.trans('cortex/foundation::common.columns').' <span class="caret"/>'],
-                ['extend' => 'pageLength', 'text' => '<i class="fa fa-list-ol"></i> '.trans('cortex/foundation::common.limit').' <span class="caret"/>'],
-            ],
-            'drawCallback' => 'function (settings) {
-                var lastGroup = null;
-                var api = this.api();
-                var colspan = api.columns(\':visible\').count();
-                var rows = api.rows({page:\'current\'}).nodes();
-
-                api.column(\'group:name\', {page:\'current\'} ).data().each(function (rowGroup, rowIndex) {
-                    if (lastGroup !== rowGroup) {
-                        $(rows).eq(rowIndex).before(
-                            \'<tr class="attribute-group"><td colspan="\'+colspan+\'"><strong>\'+(rowGroup ? rowGroup : "No Group")+\'</strong></td></tr>\'
-                        );
-     
-                        lastGroup = rowGroup;
-                    }
-                });
-            }',
-        ];
     }
 
     /**
