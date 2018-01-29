857681a5bc5c58554dc4c4e54c75acaea840bc7e
Abdelrahman Omran <me@omranic.com>
2018-01-29 16:43:05 +0200

    Simplify IoC binding

--- src/Providers/AttributesServiceProvider.php
+++ src/Providers/AttributesServiceProvider.php
@@ -103,9 +103,7 @@ class AttributesServiceProvider extends ServiceProvider
     {
         // Register artisan commands
         foreach ($this->commands as $key => $value) {
-            $this->app->singleton($value, function ($app) use ($key) {
-                return new $key();
-            });
+            $this->app->singleton($value, $key);
         }
 
         $this->commands(array_values($this->commands));
