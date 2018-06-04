04814f8710eee679733f61940b28eae04ab59dfd
Abdelrahman Omran <me@omranic.com>
2018-06-04 14:40:01 +0200

    Tweak confirmation modals

--- resources/views/adminarea/pages/attribute.blade.php
+++ resources/views/adminarea/pages/attribute.blade.php
@@ -14,7 +14,7 @@
 @section('content')
 
     @if($attribute->exists)
-        @include('cortex/foundation::common.partials.confirm-deletion')
+        @include('cortex/foundation::common.partials.modal', ['id' => 'delete-confirmation'])
     @endif
 
     <div class="content-wrapper">
@@ -26,7 +26,17 @@
         <section class="content">
 
             <div class="nav-tabs-custom">
-                @if($attribute->exists && $currentUser->can('delete', $attribute)) <div class="pull-right"><a href="#" data-toggle="modal" data-target="#delete-confirmation" data-modal-action="{{ route('adminarea.attributes.destroy', ['attribute' => $attribute]) }}" data-modal-title="{!! trans('cortex/foundation::messages.delete_confirmation_title') !!}" data-modal-body="{!! trans('cortex/foundation::messages.delete_confirmation_body', ['resource' => trans('cortex/attributes::common.attribute'), 'identifier' => $attribute->name]) !!}" title="{{ trans('cortex/foundation::common.delete') }}" class="btn btn-default" style="margin: 4px"><i class="fa fa-trash text-danger"></i></a></div> @endif
+                @if($attribute->exists && $currentUser->can('delete', $attribute))
+                    <div class="pull-right">
+                        <a href="#" data-toggle="modal" data-target="#delete-confirmation"
+                           data-modal-action="{{ route('adminarea.attributes.destroy', ['attribute' => $attribute]) }}"
+                           data-modal-title="{!! trans('cortex/foundation::messages.delete_confirmation_title') !!}"
+                           data-modal-button="<a href='#' class='btn btn-danger' data-form='delete' data-token='{{ csrf_token() }}'><i class='fa fa-trash-o'></i> {{ trans('cortex/foundation::common.delete') }}</a>"
+                           data-modal-body="{!! trans('cortex/foundation::messages.delete_confirmation_body', ['resource' => trans('cortex/attributes::common.attribute'), 'identifier' => $attribute->name]) !!}"
+                           title="{{ trans('cortex/foundation::common.delete') }}" class="btn btn-default" style="margin: 4px"><i class="fa fa-trash text-danger"></i>
+                        </a>
+                    </div>
+                @endif
                 {!! Menu::render('adminarea.attributes.tabs', 'nav-tab') !!}
 
                 <div class="tab-content">
