1903bcba9cac5c578fb3f5335709ed044ddb5b44
Abdelrahman Omran <me@omranic.com>
2018-07-24 22:52:34 +0200

    Enforce consistent resourceful routes

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -17,8 +17,9 @@ Route::domain(domain())->group(function () {
                  Route::get('import/logs')->name('import.logs')->uses('AttributesController@importLogs');
                  Route::get('create')->name('create')->uses('AttributesController@create');
                  Route::post('create')->name('store')->uses('AttributesController@store');
-                 Route::get('{attribute}')->name('edit')->uses('AttributesController@edit');
-                 Route::put('{attribute}')->name('update')->uses('AttributesController@update');
+                 Route::get('{attribute}')->name('show')->uses('AttributesController@show');
+                 Route::get('{attribute}/edit')->name('edit')->uses('AttributesController@edit');
+                 Route::put('{attribute}/edit')->name('update')->uses('AttributesController@update');
                  Route::get('{attribute}/logs')->name('logs')->uses('AttributesController@logs');
                  Route::delete('{attribute}')->name('destroy')->uses('AttributesController@destroy');
              });
