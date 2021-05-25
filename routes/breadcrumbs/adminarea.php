c4c3ebe339bb6ffc32cc399bc962b20a70e93b5f
Abdelrahman Omran <me@omranic.com>
2021-05-25 17:56:29 +0400

    Replace deprecated `Breadcrumbs::register` with `Breadcrumbs::for`

--- routes/breadcrumbs/adminarea.php
+++ routes/breadcrumbs/adminarea.php
@@ -6,33 +6,33 @@ use Diglactic\Breadcrumbs\Generator;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Cortex\Attributes\Models\Attribute;
 
-Breadcrumbs::register('adminarea.cortex.attributes.attributes.index', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.attributes.attributes.index', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-dashboard"></i> '.config('app.name'), route('adminarea.home'));
     $breadcrumbs->push(trans('cortex/attributes::common.attributes'), route('adminarea.cortex.attributes.attributes.index'));
 });
 
-Breadcrumbs::register('adminarea.cortex.attributes.attributes.import', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.attributes.attributes.import', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.attributes.attributes.index');
     $breadcrumbs->push(trans('cortex/attributes::common.import'), route('adminarea.cortex.attributes.attributes.import'));
 });
 
-Breadcrumbs::register('adminarea.cortex.attributes.attributes.import.logs', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.attributes.attributes.import.logs', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.attributes.attributes.index');
     $breadcrumbs->push(trans('cortex/attributes::common.import'), route('adminarea.cortex.attributes.attributes.import'));
     $breadcrumbs->push(trans('cortex/attributes::common.logs'), route('adminarea.cortex.attributes.attributes.import.logs'));
 });
 
-Breadcrumbs::register('adminarea.cortex.attributes.attributes.create', function (Generator $breadcrumbs) {
+Breadcrumbs::for('adminarea.cortex.attributes.attributes.create', function (Generator $breadcrumbs) {
     $breadcrumbs->parent('adminarea.cortex.attributes.attributes.index');
     $breadcrumbs->push(trans('cortex/attributes::common.create_attribute'), route('adminarea.cortex.attributes.attributes.create'));
 });
 
-Breadcrumbs::register('adminarea.cortex.attributes.attributes.edit', function (Generator $breadcrumbs, Attribute $attribute) {
+Breadcrumbs::for('adminarea.cortex.attributes.attributes.edit', function (Generator $breadcrumbs, Attribute $attribute) {
     $breadcrumbs->parent('adminarea.cortex.attributes.attributes.index');
     $breadcrumbs->push(strip_tags($attribute->name), route('adminarea.cortex.attributes.attributes.edit', ['attribute' => $attribute]));
 });
 
-Breadcrumbs::register('adminarea.cortex.attributes.attributes.logs', function (Generator $breadcrumbs, Attribute $attribute) {
+Breadcrumbs::for('adminarea.cortex.attributes.attributes.logs', function (Generator $breadcrumbs, Attribute $attribute) {
     $breadcrumbs->parent('adminarea.cortex.attributes.attributes.index');
     $breadcrumbs->push(strip_tags($attribute->name), route('adminarea.cortex.attributes.attributes.edit', ['attribute' => $attribute]));
     $breadcrumbs->push(trans('cortex/attributes::common.logs'), route('adminarea.cortex.attributes.attributes.logs', ['attribute' => $attribute]));
